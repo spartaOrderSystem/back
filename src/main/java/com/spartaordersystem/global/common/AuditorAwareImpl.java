0638e67d1a15c1668aaf247c013a9a52d14fe73d
dmlal <opkl8781@naver.com>
2024-11-07 15:55:15 +0900

    [feat] AuditorAware 구현

---
+++ src/main/java/com/spartaordersystem/global/common/AuditorAwareImpl.java
@@ -0,0 +1,17 @@
+package com.spartaordersystem.global.common;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getName);
+    }
+}
