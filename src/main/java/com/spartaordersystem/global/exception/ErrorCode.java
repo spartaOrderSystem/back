2195eab10a3aee4557efc6f8ffdb27b99c4e2c78
dmlal <opkl8781@naver.com>
2024-11-07 18:54:35 +0900

    [feat] ErrorCode, StoreStatus

--- src/main/java/com/spartaordersystem/global/exception/ErrorCode.java
+++ src/main/java/com/spartaordersystem/global/exception/ErrorCode.java
@@ -19,6 +19,8 @@ public enum ErrorCode {
 
     /*  404 NOT_FOUND : Resource 권한 없음, Resource 를 찾을 수 없음  */
     ACCESS_DENIED(404, "접근 권한이 없습니다."),
+    USER_NOT_FOUND(404, "유저를 찾을 수 없습니다."),
+    CATEGORY_NOT_FOUND(404, "카테고리를 찾을 수 없습니다."),
 
     /*  408 REQUEST_TIMEOUT : 요청에 대한 응답 시간 초과  */
     TIMEOUT_ERROR(408, "응답시간을 초과하였습니다."),
