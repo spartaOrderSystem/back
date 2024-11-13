c6cfc15841b3f0578cccda35378fe40bfcd20457
jeeheaG <jeehea99@naver.com>
2024-11-13 16:22:48 +0900

    Edit : fix role static vales

--- src/main/java/com/spartaordersystem/global/common/GlobalConst.java
+++ src/main/java/com/spartaordersystem/global/common/GlobalConst.java
@@ -4,9 +4,9 @@ public final class GlobalConst {
     public static final String TIME_ZONE_ID = "Asia/Seoul";
     public static final String TIME_ZONE_PATTERN = "yyyy/MM/dd HH:mm:ss";
 
-    public static final String ROLE_USER = "USER";
-    public static final String ROLE_OWNER = "OWNER";
-    public static final String ROLE_MANAGER = "MANAGER";
-    public static final String ROLE_MASTER = "MASTER";
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_OWNER = "ROLE_OWNER";
+    public static final String ROLE_MANAGER = "ROLE_MANAGER";
+    public static final String ROLE_MASTER = "ROLE_MASTER";
 
 }
