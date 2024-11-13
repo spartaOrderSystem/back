2814e0ad6718d2675a66a57a6ba009a020881669
dmlal <opkl8781@naver.com>
2024-11-14 00:23:00 +0900

    [feat]  ai 연결 클라이언트 구현 #38

---
+++ src/main/java/com/spartaordersystem/domains/ai/client/GeminiApiClient.java
@@ -0,0 +1,37 @@
+package com.spartaordersystem.domains.ai.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+
+@Component
+public class GeminiApiClient {
+
+    @Value("${gemini.api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent";
+
+    public String createDescription(String title, long price, String details) {
+        String prompt = String.format("제목: %s, 가격: %d. %s. 답변은 최대한 간결하게 50자 이내로 해줘", title, price, details);
+
+        String requestBody = String.format(
+                "{\"prompt\": \"%s\", \"maxTokens\":50, \"temperature\": 0.8}", prompt
+        );
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Content-Type", "application/json");
+        httpHeaders.set("Authorization", "Bearer" + apiKey);
+
+        HttpEntity<String> request = new HttpEntity<>(requestBody, httpHeaders);
+
+        ResponseEntity<String> response = restTemplate.exchange(GEMINI_API_URL, HttpMethod.POST, request, String.class);
+        return response.getBody();
+    }
+}
