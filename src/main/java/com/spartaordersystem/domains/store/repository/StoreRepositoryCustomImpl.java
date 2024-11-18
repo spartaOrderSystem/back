bc10ab4443862bd0dc3a66113fc611e5b0427da6
jeeheaG <jeehea99@naver.com>
2024-11-18 18:01:42 +0900

    Chore : remove useless code

--- src/main/java/com/spartaordersystem/domains/store/repository/StoreRepositoryCustomImpl.java
+++ src/main/java/com/spartaordersystem/domains/store/repository/StoreRepositoryCustomImpl.java
@@ -1,6 +1,5 @@
 package com.spartaordersystem.domains.store.repository;
 
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -10,14 +9,13 @@ import com.spartaordersystem.domains.store.enums.StoreStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
 
 import static com.spartaordersystem.domains.store.entity.QStore.store;
 
 
-public class StoreRepositoryCustomImpl implements StoreRepositoryCustom { //extends QuerydslRepositorySupport ? 생성자의 Class 를 찾을 수 없다 함
+public class StoreRepositoryCustomImpl implements StoreRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     public StoreRepositoryCustomImpl(JPAQueryFactory queryFactory) {
@@ -26,7 +24,6 @@ public class StoreRepositoryCustomImpl implements StoreRepositoryCustom { //exte
 
 
     @Override
-//    public Page<Store> getStoresBySearchOptions(Pageable pageable, String status, String categoryId) {
     public Page<Store> getStoresBySearchOptions(Pageable pageable, String title, StoreStatus status, Category category) {
         JPQLQuery<Store> query = queryFactory.selectFrom(store)
                 .where(containsTitle(title), eqStatus(status), eqCategory(category))
@@ -36,7 +33,6 @@ public class StoreRepositoryCustomImpl implements StoreRepositoryCustom { //exte
         List<Store> stores = query.fetch();
         return new PageImpl<Store>(stores, pageable, query.fetchCount());
 
-//        Page<Store> stores = this.getQuerydsl().applyPagination(pageable, query).fetch(); //extends QuerydslRepositorySupport
 
     }
 
