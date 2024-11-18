e0a4a8d2cb66b83f38af29ac68a6bd7731d23365
jeeheaG <jeehea99@naver.com>
2024-11-18 17:27:56 +0900

    Add : make dynamic query repository files

---
+++ src/main/java/com/spartaordersystem/domains/store/repository/StoreRepositoryCustomImpl.java
@@ -0,0 +1,63 @@
+package com.spartaordersystem.domains.store.repository;
+
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.spartaordersystem.domains.category.entity.Category;
+import com.spartaordersystem.domains.store.entity.Store;
+import com.spartaordersystem.domains.store.enums.StoreStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+import static com.spartaordersystem.domains.store.entity.QStore.store;
+
+
+public class StoreRepositoryCustomImpl implements StoreRepositoryCustom { //extends QuerydslRepositorySupport ? 생성자의 Class 를 찾을 수 없다 함
+    private final JPAQueryFactory queryFactory;
+
+    public StoreRepositoryCustomImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+
+    @Override
+//    public Page<Store> getStoresBySearchOptions(Pageable pageable, String status, String categoryId) {
+    public Page<Store> getStoresBySearchOptions(Pageable pageable, String title, StoreStatus status, Category category) {
+        JPQLQuery<Store> query = queryFactory.selectFrom(store)
+                .where(containsTitle(title), eqStatus(status), eqCategory(category))
+                .offset(pageable.getOffset()) // 페이지 번호 - 0부터 시작
+                .limit(pageable.getPageSize()) // 페이지 사이즈
+                .orderBy(store.createdAt.desc());
+        List<Store> stores = query.fetch();
+        return new PageImpl<Store>(stores, pageable, query.fetchCount());
+
+//        Page<Store> stores = this.getQuerydsl().applyPagination(pageable, query).fetch(); //extends QuerydslRepositorySupport
+
+    }
+
+    private BooleanExpression containsTitle(String title) {
+        if (title == null) {
+            return null;
+        }
+        return store.title.containsIgnoreCase(title);
+    }
+
+    private BooleanExpression eqCategory(Category category) {
+        if (category == null) {
+            return null;
+        }
+        return store.category.eq(category);
+    }
+
+    private BooleanExpression eqStatus(StoreStatus status) {
+        if (status == null) { // || status.isBlank()
+            return null;
+        }
+        return store.storeStatus.eq(status);
+    }
+}
